57a2243473cdfab3c4adab5e060fb754303a53b7
JustDMare <just@dmare.me>
2023-11-04 19:44:24 +0100

    feat: added started translation messages

--- src/i18n/en-US/index.ts
+++ src/i18n/en-US/index.ts
@@ -1,3 +1,9 @@
 export default {
   title: 'CTWorks - Technical Test',
+  table: {
+    fieldLabel: 'Field label',
+    dataValue: 'Data value',
+    addRow: 'Add row'
+  },
+  exportBtn: 'Export PDF'
 };
